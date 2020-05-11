00f719eb1fba666d0bd908b9a5661f495c5bdc5b
Joel Robinson <44164513+JR325243@users.noreply.github.com>
2020-05-11 14:04:56 +0100

    Updated title for all pages, restyled care sheets

--- App/public/js/care-sheets.js
+++ App/public/js/care-sheets.js
@@ -52,9 +52,6 @@ function getCareSheetData(id){
             //DATA
             var html = `
             <div class="">
-                <div class="card light-green lighten-1 z-depth-1">
-                    <a class="white-text">
-                    <div class="card-content">
             `;
 
             // HI JOEL
@@ -70,13 +67,17 @@ function getCareSheetData(id){
 
             for (let [key, value] of Object.entries(snap.data())) {
                 var name = key.replace(key[0], key[0].toUpperCase());
-                html += `<p><b>${name}:</b> ${value}</p>`;
+                html += `<div class="card light-green lighten-1 z-depth-1">
+                            <div class="card-content white-text">
+                                <b>${name}</b>
+                                <p>${value}</p>
+                            </div>
+                        </div>`;
+                                
             }
 
             html += `
-                </div>
-                </a>
-                </div>
+                
             </div>`;
 
             //append
